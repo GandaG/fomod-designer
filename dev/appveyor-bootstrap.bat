5908ac5c1f6e4a3e23c2d79ea35fe66fffe8128e
Daniel Nunes <gandaganza@gmail.com>
2016-04-03 17:09:04 +0000

    Fixed activate command in batch file.

--- dev/appveyor-bootstrap.bat
+++ dev/appveyor-bootstrap.bat
@@ -1,9 +1,11 @@
+@echo off
+
 set PATH=C:\Miniconda-x64;C:\Miniconda-x64\Scripts;%PATH%
 
 conda create -y -n fomod-editor^
  -c https://conda.anaconda.org/mmcauliffe -c https://conda.anaconda.org/anaconda^
   pyqt5=5.5.1 python=3.5.1 lxml=3.5.0
-activate fomod-editor
+call activate fomod-editor
 
 pip install pip -U
 pip install setuptools -U --ignore-installed
