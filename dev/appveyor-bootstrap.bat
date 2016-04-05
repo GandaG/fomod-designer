e6b171e0f8199e2f7d8b298d20795fc8f444d2cf
Daniel Nunes <gandaganza@gmail.com>
2016-04-05 14:42:57 +0000

    Fixed appveyor artifact deployment.

--- dev/appveyor-bootstrap.bat
+++ dev/appveyor-bootstrap.bat
@@ -3,8 +3,9 @@
 set PATH=C:\Miniconda-x64;C:\Miniconda-x64\Scripts;%PATH%
 
 conda create -y -n fomod-editor^
- -c https://conda.anaconda.org/mmcauliffe -c https://conda.anaconda.org/anaconda^
-  pyqt5=5.5.1 python=3.5.1 lxml=3.5.0
+ -c https://conda.anaconda.org/mmcauliffe^
+ -c https://conda.anaconda.org/anaconda^
+ pyqt5=5.5.1 python=3.5.1 lxml=3.5.0
 call activate fomod-editor
 
 pip install pip -U
