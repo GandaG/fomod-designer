06337a6206756a2a9e87f28618d849f56fa89fd0
Daniel Nunes <gandaganza@gmail.com>
2016-04-12 14:51:56 +0000

    Moved build commands to separate script.

--- dev/travis-build.sh
+++ dev/travis-build.sh
@@ -14,40 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# get pyenv - I hate messing with system python on ubuntu
-
-git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
-
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-
-
-# get pyenv-virtualenv instead of plain old virtualenv
-
-git clone https://github.com/yyuu/pyenv-virtualenv.git \
- $HOME/.pyenv/plugins/pyenv-virtualenv
-
 eval "$(pyenv virtualenv-init -)"
 
-
-# start installing the python versions
-
-env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install miniconda3-3.19.0
-
-
-# make the virtualenv
-
-pyenv shell miniconda3-3.19.0
-conda create -y -n fomod-editor \
- -c https://conda.anaconda.org/mmcauliffe \
- -c https://conda.anaconda.org/anaconda \
- python=3.5.1 pyqt5=5.5.1 lxml=3.5.0
 pyenv shell miniconda3-3.19.0/envs/fomod-editor
 
-
-# install the pip reqs
-
-pip install pip -U
-pip install setuptools -U --ignore-installed
-pip install -r dev/reqs.txt
+invoke build
