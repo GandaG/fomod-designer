f2ab4afaefd6afbd08126f032f32cc694da95b65
Daniel Nunes <gandaganza@gmail.com>
2016-04-16 18:35:32 +0000

    Fixes #10. Build command now outputs a single executable within an archive.

--- tasks.py
+++ tasks.py
@@ -47,14 +47,21 @@ def clean():
 
     rmtree("dist", ignore_errors=True)
     rmtree("build", ignore_errors=True)
+    print("Build caches cleaned.")
 
 
 @task(clean)
 def build():
     from platform import system, architecture
-    from shutil import make_archive, move
-    from os import path, curdir, environ
+    from shutil import copy
+    from os import path, curdir, environ, listdir, remove
     from configparser import ConfigParser
+    from zipfile import ZipFile
+    from fnmatch import fnmatch
+
+    # set which files will be included within the archive.
+    included_files = ["LICENSE", "README.md", "CHANGELOG.md", "CONTRIBUTING.md"]
+    archive_name = "designer"  # the archive's name
 
     try:
         build_number = environ["APPVEYOR_BUILD_NUMBER"]
@@ -72,14 +79,28 @@ def build():
     with open('setup.cfg', 'w') as configfile:
         config.write(configfile)
 
-    version = config.get('bumpversion', 'current_version') + "." + build_number
+    version = config.get('bumpversion', 'current_version') + "." + str(build_number)
+    print("Version: " + version)
 
-    spec_file = "build-{}.spec".format(system().lower())
-    spec_dir = path.join("dev", spec_file)
-    zip_name = "designer-{}-{}_{}".format(version, system().lower(), architecture()[0])
+    spec_dir = path.join("dev", "pyinstaller-build.spec")
+    zip_name = "{}-{}-{}_{}.zip".format(archive_name, version, system().lower(), architecture()[0])
     zip_dir = path.join(curdir, "dist")
+    print("Building to \"{}\"".format(path.join(zip_dir, zip_name)))
 
+    run("pyinstaller {}".format(spec_dir))
+    for item in included_files:
+        copy(item, "dist")
+        print("Copying {} to dist/".format(item))
 
-    run("pyinstaller -w --clean {}".format(spec_dir))
-    make_archive(zip_name, "zip", base_dir="FOMOD Designer", root_dir=zip_dir)
-    move(zip_name + ".zip", "dist")
+    with ZipFile(path.join(zip_dir, zip_name), "w") as zipfile:
+        for item in listdir(zip_dir):
+            if item == zip_name:
+                continue
+            zipfile.write(path.join(zip_dir, item), arcname=path.basename(path.join(zip_dir, item)))
+            print("Adding {} to the archive.".format(item))
+
+    for item in listdir(zip_dir):
+        if fnmatch(item, "*.zip"):
+            continue
+        remove(path.join(zip_dir, item))
+    print("Dist directory cleaned up.")
