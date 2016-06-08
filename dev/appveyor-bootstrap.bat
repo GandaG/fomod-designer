0f611d1ab9496699eb1b6d3999033e20a8715ecb
Daniel Nunes <gandaganza@gmail.com>
2016-06-08 15:25:46 +0000

    Upgraded validator, downgraded PyInstaller, fixed readme badges.

--- dev/appveyor-bootstrap.bat
+++ dev/appveyor-bootstrap.bat
@@ -4,7 +4,6 @@ set PATH=C:\Miniconda-x64;C:\Miniconda-x64\Scripts;%PATH%
 
 conda create -y -n fomod-designer^
  -c https://conda.anaconda.org/mmcauliffe^
- -c https://conda.anaconda.org/anaconda^
  pyqt5=5.5.1 python=3.5.1 lxml=3.5.0
 call activate fomod-designer
 
